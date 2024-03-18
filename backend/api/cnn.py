90529742a97bb1eb64440cd8ffb98ccd78db705e
Erkhes N <erkhes.nyamsaikhan@gmail.com>
2024-03-19 01:45:38 +0900

    add ml stuff

---
+++ backend/api/cnn.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(
+            in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=0
+        )
+        self.relu1 = nn.ReLU()
+
+        # Max pool 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Convolution 2
+        self.cnn2 = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0
+        )
+        self.relu2 = nn.ReLU()
+
+        # Max pool 2
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+
+        # Fully connected 1
+        self.fc1 = nn.Linear(32 * 4 * 4, 10)
+
+    def forward(self, x):
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        # Convolution 2
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2
+        out = self.maxpool2(out)
+
+        # flatten
+        out = out.view(out.size(0), -1)
+
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
+
+
+cnn_model = CNNModel()
+cnn_model.load_state_dict(torch.load("model.pt"))
+cnn_model.eval()
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from PIL import Image
 import numpy as np
+import torch
+
+from .cnn import cnn_model
 
 
 @csrf_exempt
@@ -22,6 +25,21 @@ def recognize(request):
     img = img.resize((28, 28)).convert("L")
     img = Image.fromarray(255 - np.array(img))
     img.save("output.png")
+
+    img_features = np.array(img, dtype=np.float32) / 255
+    img_features = torch.from_numpy(img_features)
+    img_features = img_features.unsqueeze(0).unsqueeze(0)
+
+    # print(img_features)
+    # print(img_features.shape)
+    with torch.no_grad():
+        outputs = cnn_model(img_features)
+
+    # Get the predicted class
+    predicted_class = torch.argmax(outputs, dim=1).item()
+
+    # Print the predicted class
+    print("Predicted class:", predicted_class)
 
     return JsonResponse(
         status=200,
